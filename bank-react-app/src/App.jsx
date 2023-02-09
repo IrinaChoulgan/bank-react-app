f87e70b5093675f7075cd816c3e1a84259a352e1
IrinaChoulgan <irina_choulgan@icloud.com>
2023-02-09 13:47:48 +0100

    add new components
    -finished Hero component
    -add Business, Button, Stats components

--- bank-react-app/src/App.jsx
+++ bank-react-app/src/App.jsx
@@ -28,9 +28,16 @@ const App = () => (
       </div>
     </div>
 
-    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
-        <Stats /> <Business /> <Billing /> <CardDeal /> <Testimonials /> <Clients /> <CTA /> <Footer />
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer />
       </div>
     </div>
   </div>
